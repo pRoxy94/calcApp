54f011dd0b9ca377ed9c67ed04a66fca88902e49
proxy94 <rossana.laricchia@gmail.com>
2020-06-08 13:08:15 +0200

    Initial Commit

---
+++ calcApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calcApp
+//
+//  Created by Rossana Laricchia on 08/06/2020.
+//  Copyright © 2020 Laricchia Rossana . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
