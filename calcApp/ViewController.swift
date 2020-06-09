e770d53c6a8c583293297fd93a474aa9d96a6d5e
proxy94 <rossana.laricchia@gmail.com>
2020-06-09 11:54:14 +0200

    Created interface, useful items added and connected to the view controller.

--- calcApp/ViewController.swift
+++ calcApp/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtWidth: UITextField!
+    @IBOutlet weak var txtHeight: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func calcArea(_ sender: Any) {
+        let w = txtWidth.text
+        let h = txtHeight.text
+        if w != nil {
+            if h != nil {
+                
+            }
+        }
+    }
+    
+    
+    
+    @IBAction func calcPerimeter(_ sender: Any) {
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+    }
+    
+    
+    
 }
 
