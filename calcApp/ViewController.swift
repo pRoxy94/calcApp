d1a33f0849a705804a9c6ce9eae295ebc91dbdfc
proxy94 <rossana.laricchia@gmail.com>
2020-06-09 17:57:47 +0200

    Interface created, useful elements added and connected to the view controller.

--- calcApp/ViewController.swift
+++ calcApp/ViewController.swift
@@ -28,27 +28,31 @@ class ViewController: UIViewController {
         calc(what: "perimeter")
     }
     
+//  method of field cleaning
     @IBAction func clear(_ sender: Any) {
         txtHeight?.text = ""
         txtWidth?.text = ""
         lblResult?.text = "0"
     }
     
+//  method to make keyboard disappear when you click out of the textfields
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
     
+//   method to calculate area or perimeter
     func calc(what:String) {
+        // NumberFormatter () --> used to convert a number into a human-readable string with a specified format. This formatter is especially useful when we want to display a currency string since it will convert the number to the proper currency for the current locale.
         let w = NumberFormatter().number(from: txtWidth.text ?? "0") as? Double
         
         let h = NumberFormatter().number(from: txtHeight.text ?? "0") as? Double
         
         if w != nil && h != nil {
             let result = what == "area" ? w!*h! : (w!+h!)*2
+            // method to format the string - only two digits after the comma
             lblResult.text = String.localizedStringWithFormat("%1.2f", result)
         } else {
             lblResult.text = "Wrong values"
         }
     }
 }
-
